654034b22d3f483fedf825ea5c27855284859213
Gonzalo Rivero <griverorz@gmail.com>
2016-05-24 09:57:55 -0400

    Remove odd test

--- tests/testthat/test-setters-getters.R
+++ tests/testthat/test-setters-getters.R
@@ -3,6 +3,5 @@ context("Setters and getters")
 test_that("Setters", {
     x <- nps(0:10)
     expect_error(top(x) <- 9, "Values outside the range")
-    expect_error(bottom(x) <- -1, "Values outside the range") ## Not sure about this
 })
 
